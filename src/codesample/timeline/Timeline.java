474c9556fb6d85c59785c020f06412eb757551ed
shyguy <suppusmac@gmail.com>
2013-07-16 10:02:22 -0400

    comment

--- src/codesample/timeline/Timeline.java
+++ src/codesample/timeline/Timeline.java
@@ -266,7 +266,9 @@ public class Timeline implements Collection<Event>
 		@Override
         public int nextIndex() //of the all the events or just the events in a particular set.
 		{
+			//Macon
 			return 0;
+			
         }
 
 		
